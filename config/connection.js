f91dc29f8752f219618bca08981959dfb4c98cbc
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-11-04 03:36:46 -0500

    added back the DB_URL to connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,20 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_URL) {
+  // Use DB_URL for deployment
   sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
   });
 } else {
+  // Fallback for local development
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
-      dialect: 'postgres'
+      dialect: 'postgres',
+      port: 5432,
     }
   );
 }
