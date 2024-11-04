fdeb3f6f371e45433edac85cecafaea13cf00ccd
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-11-04 02:37:10 -0500

    added more content to the README

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,15 @@ let sequelize;
 
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: 'postgres',
-});
+    dialect: 'postgres',
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'localhost.0.0.1',
       dialect: 'postgres'
     }
   );
